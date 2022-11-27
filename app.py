f964392dc8683d2574dbb54a9b921446f53499da
Lokeshrajkp <kplokeshraj17@gmail.com>
2022-11-27 21:22:21 +0530

    one space remved

---
+++ app.py
@@ -0,0 +1,17 @@
+import pymongo
+import pandas as pd
+import json
+client=pymongo.MongoClient("mongodb+srv://Loki100:ind100meC@cluster0.ovz7u.mongodb.net/?retryWrites=true&w=majority")
+db=Client['app']
+Collections=db['Sensor']
+Data_file_path='/config/workspace/aps_failure_training_set1.csv.1'
+if __name__=='__main__':
+    df=pd.read_csv(Data_file_path)
+    print(df.head())
+    print(f'rows & columns:{df.shape}')
+    # convert dataframe to Json to dump to Mongodb
+    df.reset_index(drop=True,inplace=True)
+    print(df)
+    json_record=list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+    client['aps']['SENSOR'].insert_many(json_record)
---
+++ data_dump.py
@@ -0,0 +1,16 @@
+import pymongo
+import pandas as pd
+import json
+# Provide the mongodb localhost url to connect python to mongodb.
+client = pymongo.MongoClient("mongodb+srv://Lokesh100:<ind100#meC>@cluster0.gb2w07a.mongodb.net/?retryWrites=true&w=majority")
+DATA_FILE_PATH='/config/workspace/aps_failure_training_set1.csv'
+DATABASE_NAME="aps"
+COLLECTION_NAME='SENSOR'
+if __name__=='__main__':
+   df=pd.read_csv(DATA_FILE_PATH)
+   print(f'rows and columns:{df.shape}')
+   # convert dataframe to JSON so that we can dump these records in Mongodb
+   df.reset_index(drop=True,inplace=True)
+   json_record=list(json.loads(df.T.to_json()).values())
+   print(json_record[0])
+   client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
